18e808cf3dbaaa9475edf33e89dc03c300c23a30
wsigma21 <wallstlbl@gmail.com>
2024-02-18 21:13:03 +0900

    add: ブログ本文の字数表示制限を追加

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -29,7 +29,7 @@ const ArticleList = ({ articles }: ArticleListProps) => {
             </Link>
             <p className="text-sm pb-3 text-slate-900">Published on {article.createdAt}</p>
             <Link href="{`articles/${article.id}`}" className="text-slate-900 pb-6">
-              {article.content}
+              {article.content.length > 70 ? article.content.substring(0,70) + "..." : article.content }
             </Link>
             <Link href="{`articles/${article.id}`}" className="text-pink-800 hover:text-black">続きを読む</Link>
           </div>
