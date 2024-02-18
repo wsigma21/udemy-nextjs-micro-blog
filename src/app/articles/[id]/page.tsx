cfd4f85380b7cb95fc3de784874bca28d30703aa
wsigma21 <wallstlbl@gmail.com>
2024-02-18 21:10:57 +0900

    add: ブログ詳細記事表示

---
+++ src/app/articles/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { getDetailArticle } from "@/blogAPI";
+import Image from "next/image";
+import React from 'react'
+
+const Article = async ({ params }: { params: { id: string } }) => {
+  const detailArticle = await getDetailArticle(params.id);
+
+  return (
+    <div className="mx-w-3xl mx-auto p-5">
+      <Image
+        src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${detailArticle.id}`}
+        alt="" 
+        width={1280}
+        height={300}
+      />
+      <h1 className="text-4xl text-center mb-10 mt-10">{detailArticle.title}</h1>
+      <div className="text-lg leading-relaxed text-justify">
+        <p>{detailArticle.content}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Article
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,13 +1,34 @@
+import { notFound } from "next/navigation";
 import { Article } from "./types";
 
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch(`http://localhost:3001/posts`, { cache: "no-store" }); // SSR
+
   if (!res.ok) {
     throw new Error("エラーが発生しました");
   }
 
   // ローディング確認用
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const articles = await res.json();
   return articles;
+}
+
+export const getDetailArticle = async (id: string): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, { 
+    next: { revalidate: 60 },
+  }); // ISR
+
+  if (res.status === 404) {
+    notFound();
+  }
+
+  if (!res.ok) {
+    throw new Error("エラーが発生しました");
+  }
+
+  // ローディング確認用
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const article = await res.json();
+  return article;
 }
