1325c5e7c888d687e1ca1c0cee27558327b79ad6
wsigma21 <wallstlbl@gmail.com>
2024-02-18 22:06:06 +0900

    feat: 削除機能

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,3 +1,4 @@
+import DeleteButton from "@/app/components/DeleteButton";
 import { getDetailArticle } from "@/blogAPI";
 import Image from "next/image";
 import React from 'react'
@@ -16,6 +17,9 @@ const Article = async ({ params }: { params: { id: string } }) => {
       <h1 className="text-4xl text-center mb-10 mt-10">{detailArticle.title}</h1>
       <div className="text-lg leading-relaxed text-justify">
         <p>{detailArticle.content}</p>
+      </div>
+      <div className="text-right mt-3">
+        <DeleteButton id={detailArticle.id}/>
       </div>
     </div>
   )
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -57,3 +57,21 @@ export const createArticle = async (
   const newArticle = await res.json();
   return newArticle;
 }
+
+export const deleteArticle = async (
+  id: string, 
+): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!res.ok) {
+    throw new Error("エラーが発生しました");
+  }
+
+  // ローディング確認用
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const deleteArticle = await res.json();
+  return deleteArticle;
+}
+
