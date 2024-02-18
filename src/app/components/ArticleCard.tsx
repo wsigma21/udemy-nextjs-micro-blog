c6e4bb917fbfb6ad4bc9b1a119558d6a5839f72b
wsigma21 <wallstlbl@gmail.com>
2024-02-18 22:23:28 +0900

    fix: リンク生成ミス修正

--- src/app/components/ArticleCard.tsx
+++ src/app/components/ArticleCard.tsx
@@ -11,7 +11,7 @@ const ArticleCard = ({ article }: ArticleCardProps) => {
   return (
     <div>
       <article className="shadow my-4 flex flex-col">
-        <Link href="{`articles/${article.id}`}" className="hover:opacity-75">
+        <Link href={`articles/${article.id}`} className="hover:opacity-75">
           <Image 
             src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
             alt="" 
@@ -23,14 +23,14 @@ const ArticleCard = ({ article }: ArticleCardProps) => {
           <Link href="#" className="text-blue-700 pb-4 font-bold">
             Technology
           </Link>
-          <Link href="{`articles/${article.id}`}" className="text-slate-900 text-3xl font-bold hover:text-gray700 pb-4">
+          <Link href={`articles/${article.id}`} className="text-slate-900 text-3xl font-bold hover:text-gray700 pb-4">
             {article.title}
           </Link>
           <p className="text-sm pb-3 text-slate-900">Published on {article.createdAt}</p>
-          <Link href="{`articles/${article.id}`}" className="text-slate-900 pb-6">
+          <Link href={`articles/${article.id}`} className="text-slate-900 pb-6">
             {article.content.length > 70 ? article.content.substring(0,70) + "..." : article.content }
           </Link>
-          <Link href="{`articles/${article.id}`}" className="text-pink-800 hover:text-black">続きを読む</Link>
+          <Link href={`articles/${article.id}`} className="text-pink-800 hover:text-black">続きを読む</Link>
         </div>
       </article>
     </div>
